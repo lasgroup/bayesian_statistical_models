af0b1d92115a5a84b89a35fb1e610ce348a26c11
sukhijab <sukhijab@student.ethz.ch>
2023-07-27 14:04:07 +0200

    added rnn model

--- bsm/utils/network_utils.py
+++ bsm/utils/network_utils.py
@@ -1,8 +1,12 @@
 from typing import Sequence, Optional, Callable
 
+import chex
 import jax
 from flax import linen as nn
+from typing import Any
 import jax.numpy as jnp
+
+Carry = Any
 
 
 class MLP(nn.Module):
@@ -22,42 +26,33 @@ class MLP(nn.Module):
 
 class GRUModel(nn.Module):
     features: Sequence[int]
+    hidden_state_size: int
+    num_cells: int
     output_dim: Optional[int]
     activation: Callable = nn.swish
-    hidden_size: int
 
     @nn.compact
-    def __call__(self, x: jax.Array, hidden_state: jax.Array, train: bool = False):
-        if train:
-            hidden_state = jnp.zeros((x.shape[0], self.hidden_size))
-        hidden_state = nn.GRUCell(hidden_state, x)
-        x = hidden_state
-        for feat in self.features:
-            x = nn.Dense(features=feat)(x)
-            x = self.activation(x)
-        if self.output_dim is not None:
-            x = nn.Dense(features=self.output_dim)(x)
-        return hidden_state, x
+    def __call__(self, inputs: jax.Array, initial_h: Optional[Carry] = None):
+        x = inputs
+        if initial_h is None:
+            initial_h = [None] * self.num_cells
+        else:
+            initial_h = jnp.split(initial_h, self.num_cells, -1)
+        rnn_hidden_state = jnp.empty((self.hidden_state_size * self.num_cells))
+        for i in range(self.num_cells):
+            final_h, full_h = nn.RNN(nn.GRUCell(features=self.hidden_state_size), return_carry=True,
+                                     name='rnn')(inputs=x,
+                                                 initial_carry=initial_h[i])
+            x = full_h
+            rnn_hidden_state = rnn_hidden_state.at[..., i * self.hidden_state_size:
+                                                        (i + 1) * self.hidden_state_size].set(final_h)
 
-
-class LSTMModel(nn.Module):
-    features: Sequence[int]
-    output_dim: Optional[int]
-    activation: Callable = nn.swish
-    hidden_size: int
-
-    @nn.compact
-    def __call__(self, x: jax.Array, hidden_state: jax.Array, cell_state: jax.Array, train: bool = False):
-        if train:
-            batch_size = x.shape[0]
-            hidden_state = jnp.zeros((batch_size, self.hidden_size))
-            cell_state = jnp.zeros((batch_size, self.hidden_size))
-        rnn_state, hidden_state = nn.LSTMCell(cell_state, hidden_state, x)
-        cell_state = rnn_state[0]
-        x = hidden_state
-        for feat in self.features:
-            x = nn.Dense(features=feat)(x)
-            x = self.activation(x)
-        if self.output_dim is not None:
-            x = nn.Dense(features=self.output_dim)(x)
-        return cell_state, hidden_state, x
+        def mlp(x: chex.Array):
+            for feat in self.features:
+                x = nn.Dense(features=feat)(x)
+                x = self.activation(x)
+            if self.output_dim is not None:
+                x = nn.Dense(features=self.output_dim)(x)
+            return x
+        x = jax.vmap(mlp)(x)
+        return rnn_hidden_state, x

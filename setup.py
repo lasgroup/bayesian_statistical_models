dd443821fab525d8f67e551768d93d34792ffefd
sukhijab <sukhijab@student.ethz.ch>
2023-07-28 11:17:22 +0200

    changed jax version in requirements

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup, find_packages
 
 required = [
     'flax>=0.4.1',
-    'jax>=0.4.13',
+    'jax==0.4.13',
     'jaxtyping>=0.2.20',
-    'jaxlib>=0.4.13',
+    'jaxlib==0.4.13',
     'pytest==7.4.0',
     'matplotlib>=3.5.1',
     'numpy>=1.22.2',
@@ -24,9 +24,21 @@ extras = {}
 setup(
     name='bsm',
     version='0.0.1',
+    license="MIT",
     packages=find_packages(),
     python_requires='>=3.9',
-    include_package_data=True,
     install_requires=required,
-    extras_require=extras
+    extras_require=extras,
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        "Development Status :: 3 - Alpha",
+        # Indicate who your project is intended for
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.11",
+    ],
 )
